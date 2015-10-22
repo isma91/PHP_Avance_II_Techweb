e0c4f327be7565c1b7efe47b8c04b0748a20bea2
larivi_n <larivi_n@epitech.eu>
2015-10-22 18:06:04 +0200

    ajout liaison projet-user

---
+++ app/cache/dev/twig/a1/a15c1a1dd66e69e3ddc1e63175cbe8404c51986ec436df5e146a461352c7a97e.php
@@ -0,0 +1,98 @@
+<?php
+
+/* NMTrelloBundle:taches:edit.html.twig */
+class __TwigTemplate_78f1f533a8804a7ca01bec4dbb356d0590884c09ef96d5c283fd7a45665dcb70 extends Twig_Template
+{
+    public function __construct(Twig_Environment $env)
+    {
+        parent::__construct($env);
+
+        // line 1
+        $this->parent = $this->loadTemplate("::base.html.twig", "NMTrelloBundle:taches:edit.html.twig", 1);
+        $this->blocks = array(
+            'body' => array($this, 'block_body'),
+        );
+    }
+
+    protected function doGetParent(array $context)
+    {
+        return "::base.html.twig";
+    }
+
+    protected function doDisplay(array $context, array $blocks = array())
+    {
+        $__internal_56b7964595008c87f4052b8e32554ae07aedc4eff0b5f73983cbfa72511e3ffd = $this->env->getExtension("native_profiler");
+        $__internal_56b7964595008c87f4052b8e32554ae07aedc4eff0b5f73983cbfa72511e3ffd->enter($__internal_56b7964595008c87f4052b8e32554ae07aedc4eff0b5f73983cbfa72511e3ffd_prof = new Twig_Profiler_Profile($this->getTemplateName(), "template", "NMTrelloBundle:taches:edit.html.twig"));
+
+        $this->parent->display($context, array_merge($this->blocks, $blocks));
+        
+        $__internal_56b7964595008c87f4052b8e32554ae07aedc4eff0b5f73983cbfa72511e3ffd->leave($__internal_56b7964595008c87f4052b8e32554ae07aedc4eff0b5f73983cbfa72511e3ffd_prof);
+
+    }
+
+    // line 3
+    public function block_body($context, array $blocks = array())
+    {
+        $__internal_f2b8f991a025c5dee657b2b0a36750753e1f2de57684016fa8672c0fea0da350 = $this->env->getExtension("native_profiler");
+        $__internal_f2b8f991a025c5dee657b2b0a36750753e1f2de57684016fa8672c0fea0da350->enter($__internal_f2b8f991a025c5dee657b2b0a36750753e1f2de57684016fa8672c0fea0da350_prof = new Twig_Profiler_Profile($this->getTemplateName(), "block", "body"));
+
+        // line 4
+        echo "<h1>taches edit</h1>
+
+    ";
+        // line 6
+        echo         $this->env->getExtension('form')->renderer->renderBlock((isset($context["edit_form"]) ? $context["edit_form"] : $this->getContext($context, "edit_form")), 'form');
+        echo "
+
+        <ul class=\"record_actions\">
+    <li>
+        <a href=\"";
+        // line 10
+        echo $this->env->getExtension('routing')->getPath("taches");
+        echo "\">
+            Back to the list
+        </a>
+    </li>
+    <li>";
+        // line 14
+        echo         $this->env->getExtension('form')->renderer->renderBlock((isset($context["delete_form"]) ? $context["delete_form"] : $this->getContext($context, "delete_form")), 'form');
+        echo "</li>
+</ul>
+";
+        
+        $__internal_f2b8f991a025c5dee657b2b0a36750753e1f2de57684016fa8672c0fea0da350->leave($__internal_f2b8f991a025c5dee657b2b0a36750753e1f2de57684016fa8672c0fea0da350_prof);
+
+    }
+
+    public function getTemplateName()
+    {
+        return "NMTrelloBundle:taches:edit.html.twig";
+    }
+
+    public function isTraitable()
+    {
+        return false;
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  58 => 14,  51 => 10,  44 => 6,  40 => 4,  34 => 3,  11 => 1,);
+    }
+}
+/* {% extends '::base.html.twig' %}*/
+/* */
+/* {% block body -%}*/
+/*     <h1>taches edit</h1>*/
+/* */
+/*     {{ form(edit_form) }}*/
+/* */
+/*         <ul class="record_actions">*/
+/*     <li>*/
+/*         <a href="{{ path('taches') }}">*/
+/*             Back to the list*/
+/*         </a>*/
+/*     </li>*/
+/*     <li>{{ form(delete_form) }}</li>*/
+/* </ul>*/
+/* {% endblock %}*/
+/* */
